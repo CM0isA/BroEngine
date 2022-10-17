9bc014cc4b98c48f8526ba6ccb49dfd5fda87d9d
cm0isa <costi.moisa98@gmail.com>
2022-10-18 00:15:57 +0300

    Configuration

--- Main.cpp
+++ Main.cpp
@@ -19,14 +19,17 @@ int main()
 	while (!window.closed())
 	{
 		window.clear();
+#if 1
 		glBegin(GL_QUADS);
 		glVertex2f(-0.5f, -0.5f);
-		glVertex2f( 0.0f,  0.5f);
+		glVertex2f(-0.5f,  0.5f);
+		glVertex2f( 0.5f,  0.5f);
 		glVertex2f( 0.5f, -0.5f);
+
 		glEnd();
-
+#else
 		glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
-
+#endif
 		window.update();
 	}
 
